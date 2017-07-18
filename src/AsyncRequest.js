ec56b4435efff1d1c706cf6f305e6cc23f1ae52e
Dan Rigsby <danrigsby@hotmail.com>
2017-07-18 15:54:15 -0400

    documented AsyncRequest

--- src/AsyncRequest.js
+++ src/AsyncRequest.js
@@ -1,3 +1,10 @@
+/**
+ * Represents an async request exposing:
+ * 1. isLoading: whether or not the operation is currently loading
+ * 2. request: data about the request that initiated the operation
+ * 3. response: the successful response of the operation (null if operation was rejected)
+ * 4. error: the error response of the operation (null if operation was successful)
+ */
 function AsyncRequest() {
   this.isLoading = false;
   this.request = null;
